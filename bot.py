27bcec2e51deb9e461cc7cc9cbb7e947679377fb
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-14 15:56:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ Bot = Client(
 db = Database()
 
 START_TEXT = """ `Hai {}, 
-Am a YouTube Downloader Bot I Can Download Songs,Videos and Lyrics From YouTube and lyrics and  Would upload into Telegram. 
+Am a song download Bot I Can Download Songs,Videos and Lyrics From YouTube and lyrics and  Would upload into Telegram. 
 Use /help Commands For More.`
 """
 
@@ -51,8 +51,8 @@ CMDS_TEXT = """
 ABOUT_TEXT = """
 - **Bot :** `Song Downloader`
 - **Creator :** [MR-JINN-OF-TG](https://Github.com/MR-JINN-OF-TG)
-- **Support :** [CLICK HERE](https://telegram.me/NAZRIYASUPPORT)
-- **Source :** [CLICK HERE](https://github.com/MR-JINN-OF-TG/Song-Downloader-Bot)
+- **Support :** [CLICK HERE](https://telegram.me/{Config.SUPPORT}")
+- **Source :** [CLICK HERE](https://t.me/IPPO_KITTUM)
 - **Language :** [Python3](https://python.org)
 - **Library :** [Pyrogram](https://pyrogram.org)
 - **Server :** [Heroku](https://heroku.com)
@@ -63,7 +63,7 @@ START_BUTTONS = InlineKeyboardMarkup(
         InlineKeyboardButton('Support📕', url=f"https://telegram.me/{Config.SUPPORT}"), 
         InlineKeyboardButton(text="SEARCH🔎", switch_inline_query_current_chat="")
         ],[
-        InlineKeyboardButton('HELP & USAGE⚙️', callback_data ='cmds') 
+        InlineKeyboardButton('HELP⚙️', callback_data ='cmds') 
         ],[
         InlineKeyboardButton('ABOUT📕', callback_data='about'),
         InlineKeyboardButton('CLOSE🔐', callback_data='close')
@@ -264,90 +264,6 @@ async def inline(client: Client, query: InlineQuery):
                 switch_pm_parameter="",
             )
         
-@Bot.on_message(filters.private & filters.command("broadcast") & filters.reply)
-async def broadcast_(c, m):
-    print("broadcasting......")
-    if m.from_user.id not in Config.OWNER_ID:
-        await c.delete_messages(
-            chat_id=m.chat.id,
-            message_ids=m.message_id,
-            revoke=True
-        )
-        return
-    all_users = await db.get_all_users()
-    broadcast_msg = m.reply_to_message
-    
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    
-    out = await m.reply_text(
-        text = f"Broadcast initiated! You will be notified with log file when all the users are notified."
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    failed = 0
-    success = 0
-    
-    broadcast_ids[broadcast_id] = dict(
-        total = total_users,
-        current = done,
-        failed = failed,
-        success = success
-    )
-    
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            
-            sts, msg = await send_msg(
-                user_id = int(user['id']),
-                message = broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            
-            if sts == 400:
-                await db.delete_user(user['id'])
-            
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current = done,
-                        failed = failed,
-                        success = success
-                    )
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-    
-    await asyncio.sleep(3)
-    
-    await out.delete()
-    
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    
-    await aiofiles.os.remove('broadcast.txt')
 
 @Bot.on_message(filters.command("lyrics"))
 async def lrsearch(_, message: Message):  
